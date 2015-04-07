4b0b18a5c3989c1db165a8f5bc3f31b37ae8300a
Chris Throup <chris@throup.org.uk>
2015-04-07 23:26:14 +0100

    Simple tests to count variable occurrences.

--- src/Analyser.php
+++ src/Analyser.php
@@ -2,7 +2,28 @@
 
 namespace CodeCloud;
 
+use PhpParser\Lexer;
+use PhpParser\Parser;
+
 
 class Analyser {
+    /**
+     * @param  string      $code
+     *
+     * @return array|int[]
+     */
+    public function analyse($code) {
+        $parser = new Parser(new Lexer);
+        $tokens = $parser->parse($code);
 
+        $analysed = [];
+        foreach ($tokens as $node) {
+            $name = $node->name;
+            if (!array_key_exists($name, $analysed)) {
+                $analysed[$name] = 0;
+            }
+            $analysed[$name]++;
+        }
+        return $analysed;
+    }
 }
--- tests/AnalyserTest.php
+++ tests/AnalyserTest.php
@@ -9,7 +9,59 @@ class AnalyserTest extends PHPUnit_Framework_TestCase {
     /**
      * @test
      */
-    public function instantiate() {
-        new Analyser();
+    public function countsNothingInEmptyCode() {
+        $analyser = new Analyser();
+
+        $code = '';
+
+        $expected = [];
+
+        $this->assertEquals($expected, $analyser->analyse($code));
+    }
+
+    /**
+     * @test
+     */
+    public function countsSingleVariable() {
+        $analyser = new Analyser();
+
+        $code = '<?php $variable;';
+
+        $expected = [
+            'variable' => 1,
+        ];
+
+        $this->assertEquals($expected, $analyser->analyse($code));
+    }
+
+    /**
+     * @test
+     */
+    public function countsTwoInstancesOfSingleVariable() {
+        $analyser = new Analyser();
+
+        $code = '<?php $variable; $variable;';
+
+        $expected = [
+            'variable' => 2,
+        ];
+
+        $this->assertEquals($expected, $analyser->analyse($code));
+    }
+
+    /**
+     * @test
+     */
+    public function countsTwoDifferentVariables() {
+        $analyser = new Analyser();
+
+        $code = '<?php $different; $variable;';
+
+        $expected = [
+            'different' => 1,
+            'variable'  => 1,
+        ];
+
+        $this->assertEquals($expected, $analyser->analyse($code));
     }
 }
