dc696d57e098aa3403fb8b9ba7c2ba4a149266f9
Chris Throup <chris@throup.org.uk>
2015-04-08 21:46:30 +0100

    Initial tests and implementation for tallying variable assignments.

--- src/Analyser.php
+++ src/Analyser.php
@@ -3,6 +3,7 @@
 namespace CodeCloud;
 
 use PhpParser\Lexer;
+use PhpParser\Node;
 use PhpParser\Parser;
 
 
@@ -16,14 +17,52 @@ class Analyser {
         $parser = new Parser(new Lexer);
         $tokens = $parser->parse($code);
 
-        $analysed = [];
+        $this->analysed = [];
         foreach ($tokens as $node) {
-            $name = $node->name;
-            if (!array_key_exists($name, $analysed)) {
-                $analysed[$name] = 0;
-            }
-            $analysed[$name]++;
+            $this->processNode($node);
         }
-        return $analysed;
+        return $this->analysed;
     }
+
+    /**
+     * @param $name
+     */
+    private function tally($name) {
+        if (!array_key_exists($name, $this->analysed)) {
+            $this->analysed[$name] = 0;
+        }
+        $this->analysed[$name]++;
+    }
+
+    /**
+     * @param Node\Expr $node
+     */
+    private function processNode(Node $node) {
+        switch (get_class($node)) {
+            case 'PhpParser\Node\Expr\Variable':
+            /** @var Node\Expr\Variable $node */
+                $this->tally($node->name);
+                break;
+
+            case 'PhpParser\Node\Expr\Assign':
+                /** @var Node\Expr\Assign $node */
+                $this->processNode($node->var);
+                $this->processNode($node->expr);
+                break;
+
+            case 'PhpParser\Node\Scalar\String_':
+            case 'PhpParser\Node\Scalar\LNumber':
+            case 'PhpParser\Node\Scalar\DNumber':
+            /**
+             * @var Node\Scalar\String_|Node\Scalar\LNumber|Node\Scalar\DNumber $node
+             */
+                $this->tally((string) $node->value);
+                break;
+
+            default:
+                throw new \Exception(var_export($node, true));
+        }
+    }
+
+    private $analysed = [];
 }
--- tests/AnalyserTest.php
+++ tests/AnalyserTest.php
@@ -64,4 +64,83 @@ class AnalyserTest extends PHPUnit_Framework_TestCase {
 
         $this->assertEquals($expected, $analyser->analyse($code));
     }
+
+    /**
+     * @test
+     */
+    public function assignOneVariableToAnother() {
+        $analyser = new Analyser();
+
+        $code = '<?php $different = $variable;';
+
+        $expected = [
+            'different' => 1,
+            'variable'  => 1,
+        ];
+
+        $this->assertEquals($expected, $analyser->analyse($code));
+    }
+
+    /**
+     * @test
+     */
+    public function assignStringToAVariable() {
+        $analyser = new Analyser();
+
+        $code = '<?php $variable = "value";';
+
+        $expected = [
+            'variable'  => 1,
+            'value'     => 1,
+        ];
+
+        $this->assertEquals($expected, $analyser->analyse($code));
+    }
+
+    /**
+     * @test
+     */
+    public function assignStringToVariableWithSameName() {
+        $analyser = new Analyser();
+
+        $code = '<?php $variable = "variable";';
+
+        $expected = [
+            'variable'  => 2,
+        ];
+
+        $this->assertEquals($expected, $analyser->analyse($code));
+    }
+
+    /**
+     * @test
+     */
+    public function assignIntegerToAVariable() {
+        $analyser = new Analyser();
+
+        $code = '<?php $variable = 1;';
+
+        $expected = [
+            'variable'  => 1,
+            '1'         => 1,
+        ];
+
+        $this->assertEquals($expected, $analyser->analyse($code));
+    }
+
+    /**
+     * @test
+     */
+    public function assignFloatToAVariable() {
+        $analyser = new Analyser();
+
+        $code = '<?php $variable = 1.2;';
+
+        $expected = [
+            'variable'  => 1,
+            '1.2'       => 1,
+        ];
+
+        $this->assertEquals($expected, $analyser->analyse($code));
+    }
 }
